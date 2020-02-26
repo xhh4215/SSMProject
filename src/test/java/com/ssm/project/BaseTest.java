1ce3815dee8d43cea2905535962a84569acbeafe
xhh <18734184215@163.com>
2020-02-26 09:43:12 +0800

    service层的实现和测试

--- src/test/java/com/ssm/project/BaseTest.java
+++ src/test/java/com/ssm/project/BaseTest.java
@@ -10,6 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 告诉junit Spring的配置文件的位置
-@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
 public class BaseTest {
 }
---
+++ src/test/java/com/ssm/project/services/AreaServicesTest.java
@@ -0,0 +1,18 @@
+package com.ssm.project.services;
+
+import com.ssm.project.BaseTest;
+import com.ssm.project.entity.Area;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AreaServicesTest extends BaseTest {
+    @Autowired
+    private AreaService areaService;
+    public  void testGetAreaList(){
+         List<Area> areaList   = areaService.getAreaList();
+         assertEquals("哈尔滨",areaList.get(0).getAreaName());
+    }
+}
