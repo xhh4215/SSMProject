9305d7ce649a6f89b84a713ab92f25e90b76e9bd
xhh <18734184215@163.com>
2020-02-26 21:28:44 +0800

    日志打印功能的实现

--- src/main/java/com/ssm/project/web/superadmin/AreaController.java
+++ src/main/java/com/ssm/project/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.ssm.project.web.superadmin;
 
 import com.ssm.project.entity.Area;
 import com.ssm.project.services.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,14 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")  //当前controller映射的路径地址
 public class AreaController {
+    Logger logger  = LoggerFactory.getLogger(AreaController.class);
     @Autowired //在需要当前类的时候spring会自动将实现了注入
     private AreaService areaService;
     @RequestMapping(value = "/listarea",method = RequestMethod.GET)
     @ResponseBody // 将结果以json的格式返回给前端
     private Map<String,Object> listArea(){
+        logger.info("==== start ====");
+        long startTime = System.currentTimeMillis();
         Map<String,Object> modelMap = new HashMap<>();
         List<Area> list = new ArrayList<>();
          try{
@@ -33,7 +38,11 @@ public class AreaController {
              modelMap.put("errMsg",e.toString());
 
          }
-  return modelMap;
+         logger.error("test error!");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime[{}ms]",endTime-startTime);
+        logger.info("==== end ====");
+        return modelMap;
     }
 
 }
